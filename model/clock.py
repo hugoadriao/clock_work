110cdf8393a442a20e3c95f88d8d7f1cd39d0563
Hugo Silva <hugovictorsilva94@gmail.com>
2020-10-26 18:12:05 -0400

    feat(model/clock): insert all needed logic to represent the clock table
    with this change is possible to properly represent the clock table

---
+++ model/clock.py
@@ -0,0 +1,24 @@
+from config.sql_alchemy import db
+
+
+class ClockModel(db.Model):
+    __tablename__ = 'clock'
+
+    cloid = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False,
+        server_default=db.FetchedValue()
+    )
+    cloregister = db.Column(db.Time, nullable=False)
+    clotimestamp = db.Column(db.DateTime, primary_key=True, nullable=False)
+    cloperid = db.Column(
+        db.ForeignKey('person.perid'),
+        primary_key=True, nullable=False
+    )
+
+    person_relation = db.relationship(
+        'Person',
+        primaryjoin='ClockModel.cloperid == Person.perid',
+        backref='clocks'
+    )
