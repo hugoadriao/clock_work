8d66003243ca9bb41747f664b773d3fcc0e442a8
Hugo Silva <hugovictorsilva94@gmail.com>
2020-10-26 18:57:43 -0400

    feat(/app): update the app file with the function to create a database if don't exist a database
    with this change the API will create a database file

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from config.app_config import app
 
 
+@app.before_first_request
+def create_database():
+    db.create_all()
+
+
 if __name__ == '__main__':
     from config.sql_alchemy import db
 
