495a01ab8dcf5968db602a1a690c1c8bfe2f80db
Hugo Silva <hugovictorsilva94@gmail.com>
2020-10-28 19:28:34 -0400

    feat(/app): create a basic route to the Clock resource
    with this route is possible to test the API with swagger

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from config.app_config import app
+from namespace import *
+from resources.clock import Clock
 
 
-@app.before_first_request
-def create_database():
-    db.create_all()
-
+clock_namespace.add_resource(
+    Clock,
+    '/clocks',
+    route_doc={
+        'description': 'This route returns hello world',
+        'responses': {
+            404: "Not found",
+            200: "Success",
+            500: "Internal Error"
+        }
+    }
+)
 
 if __name__ == '__main__':
     from config.sql_alchemy import db
