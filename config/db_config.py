051e83fcbf6524c8457548837aa5f2b9dd1fc524
Hugo Silva <hugovictorsilva94@gmail.com>
2020-10-29 20:16:32 -0400

    feat(config/db_config): insert postgresql configuration and URI
    With this change is possible to correctly connect with database

--- config/db_config.py
+++ config/db_config.py
@@ -1,1 +1,19 @@
 SQLITE_URI = 'sqlite:///database/clock_database.db'
+
+POSTGRESQL_CONFIG = {
+    'user': 'hugo',
+    'password': '1029',
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'mydb'
+}
+p_user = POSTGRESQL_CONFIG.get('user')
+p_password = POSTGRESQL_CONFIG.get('password')
+p_host = POSTGRESQL_CONFIG.get('host')
+p_port = POSTGRESQL_CONFIG.get('port')
+p_database = POSTGRESQL_CONFIG.get('database')
+
+POSTGRESQL_URI = (
+    f'postgresql+psycopg2://{p_user}:{p_password}'
+    f'@{p_host}:{p_port}/{p_database}'
+)
