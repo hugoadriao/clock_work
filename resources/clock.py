9960d79b6f1ef52194ad5219109f3b33a3188b09
Hugo Silva <hugovictorsilva94@gmail.com>
2020-10-31 17:45:08 -0400

    feat(resources/clock): update the get function of the Clock Resource
    This change introduces the max_no_break_time, worked_time, close_of_business and extra_hour
    variables

--- resources/clock.py
+++ resources/clock.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta, time
 
 from flask_restx import Resource
 
@@ -14,6 +14,24 @@ class Clock(Resource):
         all_model_registers: [ClockModel] = (
             ClockModel.get_all_registers_from_today(date_now, user_id)
         )
+
+        max_no_break_time = None
+        worked_time = None
+        close_of_business = None
+        extra_hour = None
+
+        if len(all_model_registers) == 1:
+            max_no_break_time = (
+                    all_model_registers[0].cloregister + timedelta(
+                        hours=6
+                    )
+            )
+        elif len(all_model_registers) == 2:
+
+            worked_time = (
+                    all_model_registers[1].cloregister -
+                    all_model_registers[0].cloregister
+            )
 
         all_datetime_registers: [str] = [
             str(validate_date(register.cloregister)) for register in
