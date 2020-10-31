906e3e3f2f8cd998f2d825931937e5b3bd575796
Hugo Silva <hugovictorsilva94@gmail.com>
2020-10-31 15:17:43 -0400

    feat(resources/clock): insert the needed logic to retrieve all register from the current day
    with this change is possible to access the database and extract relevant data

--- resources/clock.py
+++ resources/clock.py
@@ -1,7 +1,23 @@
+from datetime import datetime
+
 from flask_restx import Resource
+
+from model.clock import ClockModel
+from utils.generic_functions import validate_date
 
 
 class Clock(Resource):
 
     def get(self):
-        return 'hello world'
+        user_id = 2
+        date_now = datetime.now()
+        all_model_registers: [ClockModel] = (
+            ClockModel.get_all_registers_from_today(date_now, user_id)
+        )
+
+        all_datetime_registers: [str] = [
+            str(validate_date(register.cloregister)) for register in
+            all_model_registers
+        ]
+
+        return all_datetime_registers
